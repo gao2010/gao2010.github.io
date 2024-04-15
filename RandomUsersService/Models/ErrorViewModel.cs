b98397eb49fa410a5194e185dd44a8bc778effff
gao2010 <gao2010@gmail.com>
2024-04-15 16:53:58 -0500

    Updates

--- RandomUsersService/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace RandomUsersService.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
